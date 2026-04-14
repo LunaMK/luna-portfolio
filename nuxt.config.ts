c3a477c710874b3a7431fb0a147ac8c898686ca3
LunaMK <50502460+LunaMK@users.noreply.github.com>
2026-04-14 14:36:30 +0300

    fix: switch content to d1 database for cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,52 +1,62 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-    app: {
+  app: {
     head: {
       htmlAttrs: {
-        lang: 'en'
+        lang: "en",
       },
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
       ],
       link: [
-        { rel: 'icon', href: '/favicon.ico' },
+        { rel: "icon", href: "/favicon.ico" },
         // { rel: 'icon', type: 'image/x-icon', href: process.env.BASE_URL + '/logo.png' },
         {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
         },
-      ]
-    }
+      ],
+    },
   },
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui',
-    '@nuxt/content',
-    '@nuxt/image',
-    '@nuxthub/core'
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxt/content",
+    "@nuxt/image",
+    "@nuxthub/core",
   ],
   colorMode: {
-    preference: 'system',
-    fallback: 'light',
-    classSuffix: ''
+    preference: "system",
+    fallback: "light",
+    classSuffix: "",
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
-    '/': { prerender: true }
+    "/": { prerender: true },
   },
 
-  compatibilityDate: '2025-01-15',
+  compatibilityDate: "2025-01-15",
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-  
-})
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+  content: {
+    database: {
+      type: "d1",
+      bindingName: "DB", 
+    },
+  },
+  nitro: {
+    experimental: {
+      database: true,
+    },
+  },
+});
